e0f5335c55f1af44d2d040658240aa3845409183
Craig Wayne <craig.wayne01@gmail.com>
2015-08-30 19:12:00 +0200

    updates to the spark table

--- core/nexus_spark_table/nexus_spark_table.php
+++ core/nexus_spark_table/nexus_spark_table.php
@@ -2,10 +2,10 @@
 
 require_once("core/nexus_table/nexus_table.php");
 
-class nexus_spark_table extends nexus_table{
+class nexus_infinity_table extends nexus_table{
 
   function create($data = []){
-      print_r('Spark Tables use a different table structure<br/>');
+      print_r('Infinity Tables use a different table structure<br/>');
       print_r('create a table that is selectable, and dynamic, with all columns<br/>');
       //$data['content'] = $this->generate_html_table($this->columns);
       $html_table = '
@@ -13,45 +13,23 @@ class nexus_spark_table extends nexus_table{
           <thead>
             <tr>';
 
-      $grid_columns = 1;
-      $html_table .= '<th class="row_number">#</th>';
-      $spark_group_fields = [];
-      foreach($this->user_columns as $key => $column){
-        if($column["spark_group"] != true){
-          $html_table .= '<th class="'.$column["name"].'">';
-          $html_table .= $column["readable_name"];
+      foreach($this->user_columns as $column => $info){
+        if(array_key_exists('group',$info) ){
+          $html_table .= '<th>';
+          $html_table .= $column;
           $html_table .= '</th>';
-          $grid_columns++;
-        }
-        else{
-          $spark_group_fields[] = $column;
         }
       }
 
       $html_table .= '</tr>
           </thead>
-          <tbody>';
-
-      $min_rows = 5;
-      for($i=0; $i<$min_rows; $i++){
-        $html_table .= '<tr>';
-        $html_table .= '<td class="row_number"></td>';
-        foreach($this->user_columns as $key=>$column){
-          if($column["spark_group"] != true){
-            $html_table .= '<td class="'.$column["name"].'"><input /></td>';
-          }
-        }
-        $html_table .= '</tr>';
-      }
-
-      $html_table .='
+          <tbody>
           </tbody>
           <tfoot>
           </tfoot>
         </table>
       ';
-      $data['content']  = '';
-      $data['content'] .= $html_table;
+      $data['content'] = $html_table;
       return parent::create($data);
   }
 
