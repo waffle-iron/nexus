5a6097503584b47cd6d14cc0dcfe19a273bf22d7
Craig Wayne <craigiswayne@gmail.com>
2016-06-23 10:27:01 +0200

    Added GAE Symfony2
    Very basic setup

---
+++ src/AppEngine/HelloWorldBundle/Controller/DefaultController.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace AppEngine\HelloWorldBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        phpinfo();
+        $time = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
+        echo '<p>Load time: '.round($time,2).' seconds</p>';
+        return $this->render('default/index.html.twig');
+    }
+}
