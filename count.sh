e1b9c557f730e55933c6d6a9927865596a4ffe31
Craig Wayne <craigiswayne@gmail.com>
2016-06-30 16:55:16 +0200

    Successfully updated gae with new php framework

--- count.sh
+++ count.sh
@@ -3,10 +3,13 @@
 ROOT=".";
 TOTAL=$(find $ROOT -type f | wc -l);
 
-for dir in $(ls $ROOT);
+for dir in $(ls -A $ROOT);
 do
     COUNT=$(find $ROOT/$dir -type f | wc -l)
     echo $dir " :" $COUNT;
 done;
 
 echo "Total files: " $TOTAL;
+
+echo "Remember Google App Engine only allows 10,000 files to be uploaded";
+appcfg.py resource_limits_info .
--- fresh.sh
+++ fresh.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
+echo "Emptying ./vendor";
 rm -rf ./vendor/*;
-rm -rf ./node_modules//*
+
+echo "Emptying ./node_modules";
+rm -rf ./node_modules/*;
+
+echo "Emptying ./bower_components";
+rm -rf ./bower_components/*;
+
+echo "Reinstalling node packages";
 npm install;
+
+echo "Reinstalling composer packages";
 composer install;
+
+echo "Reinstalling bower packages";
 bower install;
